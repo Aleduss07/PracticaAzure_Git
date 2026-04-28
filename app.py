c7033b8e6fe2f066f71d223a9d46f0f210110ce3
Alexis <aleduss.work@gmail.com>
2026-04-28 23:23:00 +0000

    [Edicion 5]

--- app.py
+++ app.py
@@ -157,3 +157,7 @@ def enviar_alerta():
         
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
